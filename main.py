706fe0e8e7e51b9ef72a40f558ea37060b5651a0
assudh <ali.735378104a@gmail.com>
2026-07-08 17:08:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("DNS Tunneling Server is ready")
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "DNS Tunneling Server is running!"
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
+
